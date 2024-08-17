8da57d04ac3c9f7131f60471d77f4a4322c405b6
HamimBhai742 <omagootruelove147@gmail.com>
2024-08-17 16:13:29 +0600

    Last Update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <p>Home</p>
+            <h3 className='text-5xl font-semibold min-h-screen w-full flex justify-center items-center'>Comming Soon......</h3>
         </div>
     );
 };
